0c8ca391dadf1f7423b1ab88ce1056f055809693
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-09 12:04:54 -0500

    barebones

---
+++ src/components/Counter.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'; 
+
+class Counter extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <>
+                <div>hey it's the component here</div>
+            </>
+        );
+    }
+}
+
+export default Counter;
